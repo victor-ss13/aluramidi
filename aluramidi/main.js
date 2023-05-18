521f6501938118a7bdfc295bd1f6cecde5830dfc
victor-ss13 <victor.sampaio2003@gmail.com>
2023-05-18 18:05:43 -0300

    Add files via upload

---
+++ aluramidi/main.js
@@ -0,0 +1,9 @@
+//O querySelector serve para buscar um elemento dentro de uma referência (no caso desse código, a referência é o document que é nosso arquivo HTML). O querySelector pode buscar um seletor seja ele o elemento em si, sua classe ou id
+
+function
+
+
+
+document.querySelector('.tecla_pom')
+
+document.querySelector('#som_tecla_pom').play();
