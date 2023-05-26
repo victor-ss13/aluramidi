73c4d3505eeaa883aebfa966ed39f1567eb2a7b5
victor-ss13 <victor.sampaio2003@gmail.com>
2023-05-26 16:53:13 -0300

    Update main.js

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,9 +1,20 @@
 //O querySelector serve para buscar um elemento dentro de uma referência (no caso desse código, a referência é o document que é nosso arquivo HTML). O querySelector pode buscar um seletor seja ele o elemento em si, sua classe ou id
+//Criamos funções para podermos escolher quando executar o código que está dentro dela
 
-function
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
 
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+let i = 0;
 
-document.querySelector('.tecla_pom')
+while (i < listaDeTeclas.length) {
+    listaDeTeclas[i].onclick = function () {
+        tocaSom('#som_tecla_pom')
+    };
 
-document.querySelector('#som_tecla_pom').play();
+    i++;
+
+    console.log(i);
+}
